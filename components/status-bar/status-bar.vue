974ff1c135426641840dbeade3ea5be1bdda2051
llglinmx <llg1996lmx0313@163.com>
2021-08-24 17:22:50 +0800

    初次提交数据

---
+++ components/status-bar/status-bar.vue
@@ -0,0 +1,28 @@
+<template>
+	<view class="nav-bar"></view>
+</template>
+
+<script>
+	export default {
+		name: "status-bar",
+		data() {
+			return {
+
+			};
+		},
+		mounted() {
+			// uni.getSystemInfo({
+			// 	success: (res) => {
+			// 		console.log(res.statusBarHeight)
+			// 	}
+			// });
+		},
+	}
+</script>
+
+<style lang="scss" scoped>
+	.nav-bar {
+		height: var(--status-bar-height);
+		width: 100%;
+	}
+</style>
